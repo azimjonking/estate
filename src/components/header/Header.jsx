6f7b684f487fd34653db88e30b71972fc4e94535
MUhammad20092 <muhammadzohirmalikjanov47@gmail.com>
2024-10-14 15:59:15 +0500

    header tugadi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import "./Header.css";
+import logo from "./IMAGE.png";
 
 const Header = () => {
-  return <div>Header</div>;
-};
+  return <div className="first"> <img className="img1" src={logo} alt="" /> <a className="first1" href="">Listings</a> <a className="first1" href="">Newsroom</a> <a className="first1" href="">Contact</a> <a className="first1"  href="">Our team</a> </div>;
+};  
 
 export default Header;
