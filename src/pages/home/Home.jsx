7090f9387b0401632fd6da5680016b7dd91b7d79
azimjonking <azimjonjalilov1301@mail.ru>
2024-10-14 14:33:45 +0300

    estate project folder structure

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+import Header from "../../components/header/Header";
+import Main from "../../components/main/Main";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default Home;
