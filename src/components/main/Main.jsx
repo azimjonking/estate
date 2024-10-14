1de5d6521e5203294380a8f49f9bfc50586a1819
azimjonking <azimjonjalilov1301@mail.ru>
2024-10-14 15:55:11 +0300

    main section done

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,19 @@
 import "./Main.css";
+import logo from "../../assets/home/logo.png";
+import homebg from "../../assets/home/home-bg.png";
+import { FaArrowDown } from "react-icons/fa6";
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <section className="main" style={{ backgroundImage: `url(${homebg})` }}>
+      <div className="main-info">
+        <img src={logo} alt="Estate" />
+        <h1 className="home-title">We're here to help you find a new home.</h1>
+      </div>
+      <button className="home-btn">
+        <FaArrowDown />
+      </button>
+    </section>
+  );
 };
 
 export default Main;
